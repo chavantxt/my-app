ef5db419634cde8f96a8b040c57f7deb47a7fc80
chavantxt <tejaswini.chavan2727@gmail.com>
2024-05-27 11:10:17 +0530

    Implementing component and Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path:'' , component:HomeComponent},
+   {path :'list', component:ListComponent},
+
+]; 
+
+@NgModule({
+imports:[RouterModule.forRoot(routes)],
+exports:[RouterModule]
+})
+
+export class AppRoutingModule{  }
